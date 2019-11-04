0f62a538a025cf8460fe9c5b6e4f7dfdd82a2e4d
GrajaKonrad <54834591+GrajaKonrad@users.noreply.github.com>
2019-11-04 16:41:39 +0100

    zadanie dodanie próba 2

---
+++ zadanie_domowe/zadanie4.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+      td{
+        border: 1px solid black;
+        width: 40px;
+        height: 30px;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <table>
+
+
+    <?php
+      $x=0;
+      $y=0;
+      $suma=0;
+      do{
+        $x++;
+        echo '<tr>';
+        do {
+          $y++;
+          if($y+$x==11){echo '<td style="background-color:red;">',$x*$y,'</td>';
+          $suma+=$x*$y;}
+          else{echo '<td>',$x*$y,'</td>';}
+        } while ($y<10);
+        echo '</tr>';
+        $y=0;
+      }while($x<10);
+
+     ?>
+     </table>
+     <?php
+     echo 'Suma liczb po skosie wynosi: ',$suma;
+      ?>
+  </body>
+</html>
